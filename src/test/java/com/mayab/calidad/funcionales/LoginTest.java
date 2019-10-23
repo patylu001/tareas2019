73ac90e0045494ba2c77c287959f5e3b24bd7ef5
Patricia Ortegon <patricia.ortegon@ibm.com>
2019-10-22 20:40:03 -0500

    Nueva prueba login aprende

---
+++ src/test/java/com/mayab/calidad/funcionales/LoginTest.java
@@ -0,0 +1,16 @@
+package com.mayab.calidad.funcionales;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginTest {
+
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
